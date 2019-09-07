4c17e6515f1b3a2bd9d171d94298a8df1332bfdc
Shabbir khan <iamshabbirofficial@gmail.com>
2019-09-07 13:36:14 +0530

    React Single Page App

---
+++ precache-manifest.cd54bf0194a36702e457d80fc9a21577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebf615def35ec935b96f95690f7c9f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fff6bd4de593155e7e8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "265150be0e752ae94085",
+    "url": "/static/js/2.92fb8ca5.chunk.js"
+  },
+  {
+    "revision": "8fff6bd4de593155e7e8",
+    "url": "/static/js/main.2c49248a.chunk.js"
+  },
+  {
+    "revision": "474f79572f729dca25cd",
+    "url": "/static/js/runtime~main.b56cd54a.js"
+  }
+]);
